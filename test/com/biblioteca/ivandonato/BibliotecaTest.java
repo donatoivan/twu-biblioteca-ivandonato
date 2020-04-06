b181dfb9b4b768c5febb1cca974319f25996759c
Ivan Donato <donato.ivan@gmail.com>
2020-04-07 02:08:57 +1000

    [ivan][#1.2] added displayBooklist test, displayBooklist method to Biblioteca. tests passed

--- test/com/biblioteca/ivandonato/BibliotecaTest.java
+++ test/com/biblioteca/ivandonato/BibliotecaTest.java
@@ -1,16 +1,25 @@
 package com.biblioteca.ivandonato;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class BibliotecaTest {
+    public ArrayList<Book> books;
+
+    @Before
+    public void init() {
+        books = new ArrayList<>();
+        books.add(new Book("Hamlet"));
+        books.add(new Book("Romeo & Juliet"));
+        books.add(new Book("Merchant of Venice"));
+    }
 
     @Test
     public void welcomeMessageTest() {
@@ -22,8 +31,25 @@ public class BibliotecaTest {
 
         biblioteca.welcomeMessage();
 
-        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!",
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n",
                 output.toString());
+    }
+
+    @Test
+    public void displayBookList() {
+        Biblioteca biblioteca = new Biblioteca();
+        BookList booklist = new BookList();
+        booklist.setBooklist(books);
+
+        OutputStream output = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(output);
+        System.setOut(printStream);
+
+        biblioteca.displayBookList(booklist.booklist);
+
+
+        assertEquals("Hamlet\nRomeo & Juliet\nMerchant of Venice\n", output.toString());
+
     }
 
 }
