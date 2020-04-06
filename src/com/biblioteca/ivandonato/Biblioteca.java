20abc69f43537f2a72b3d1580d0834d6ecfa541c
Ivan Donato <donato.ivan@gmail.com>
2020-04-06 21:17:07 +1000

    [ivan][#1.1] added welcomeMessage to Biblioteca class to make test pass

--- src/com/biblioteca/ivandonato/Biblioteca.java
+++ src/com/biblioteca/ivandonato/Biblioteca.java
@@ -2,5 +2,6 @@ package com.biblioteca.ivandonato;
 
 public class Biblioteca {
     public void welcomeMessage() {
+        System.out.print("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
 }
