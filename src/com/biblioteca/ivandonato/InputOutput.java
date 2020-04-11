85c3abb59b8c77d9ea6d9cb902f2d0b8d0573e6f
Ivan Donato <donato.ivan@gmail.com>
2020-04-11 20:20:18 +1000

    [ivan][code refactor] refactored code according to Xiaojun's feedback which included implmenting Dependency Injection, refactoring out login in Biblioteca main, and removing setOut from tests

--- src/com/biblioteca/ivandonato/InputOutput.java
+++ src/com/biblioteca/ivandonato/InputOutput.java
@@ -1,20 +1,26 @@
 package com.biblioteca.ivandonato;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputOutput {
+    PrintStream stream;
+
+    public InputOutput(PrintStream stream) {
+        this.stream = stream;
+    }
 
     public void welcomeMessage() {
-        System.out.print("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n\n");
+        stream.print("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n\n");
     }
 
     public void displayMenu() {
-        System.out.println("[Menu Options] (Please type a number)");
-        System.out.println("[0] Quit Application");
-        System.out.println("[1] List of books");
-        System.out.println("[2] Checkout book");
-        System.out.println("[3] Return book");
+        stream.println("[Menu Options] (Please type a number)");
+        stream.println("[0] Quit Application");
+        stream.println("[1] List of books");
+        stream.println("[2] Checkout book");
+        stream.println("[3] Return book");
     }
 
     public String getInputFromUser() {
@@ -22,36 +28,40 @@ public class InputOutput {
         return scanner.nextLine();
     }
 
-    public void askForTitle() {
-        System.out.println("Please type the title of the book you wish to checkout: ");
+    public void askForCheckoutTitle() {
+        stream.println("Please type the title of the book you wish to checkout: ");
+    }
+
+    public void askForReturnTitle() {
+        stream.println("Please type the title of the book you wish to checkout: ");
     }
 
     public void inValidOption() {
-        System.out.println("Please select a valid option!");
+        stream.println("Please select a valid option!");
     }
 
     public void displayBookList(ArrayList booklist) {
         for (int i = 0; i < booklist.size(); i++) {
             Book book = (Book) booklist.get(i);
             if (book.getCheckout().equals(false)) {
-                System.out.println(String.format("%s | %s | %s", book.getTitle(), book.getAuthor(), book.getYear()));
+                stream.println(String.format("%s | %s | %s", book.getTitle(), book.getAuthor(), book.getYear()));
             }
         }
     }
 
     public void successfulCheckoutMessage() {
-        System.out.println("Thank you! Enjoy the book");
+        stream.println("Thank you! Enjoy the book");
     }
 
     public void unsuccessfulCheckoutMessage() {
-        System.out.println("Sorry, that book is not available");
+        stream.println("Sorry, that book is not available");
     }
 
     public void successfulReturnMessage() {
-        System.out.println("Thank you for returning the book");
+        stream.println("Thank you for returning the book");
     }
 
     public void unsuccessfulReturnMessage() {
-        System.out.println("That is not a valid book to return.");
+        stream.println("That is not a valid book to return.");
     }
 }
--- test/com/biblioteca/ivandonato/InputOutputTest.java
+++ test/com/biblioteca/ivandonato/InputOutputTest.java
@@ -3,10 +3,7 @@ package com.biblioteca.ivandonato;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
@@ -15,7 +12,7 @@ public class InputOutputTest {
     public ArrayList<Book> books;
 
     @Before
-    public void initilaiseBooklist() {
+    public void initialiseBookList() {
         books = new ArrayList<>();
         books.add(new Book("Hamlet", "William Shakespeare", "1603"));
         books.add(new Book("Romeo & Juliet", "William Shakespeare", "1597"));
@@ -25,53 +22,35 @@ public class InputOutputTest {
 
     @Test
     public void shouldDisplayWelcomeMessageTest() {
-        InputOutput inputOutput = new InputOutput();
-        OutputStream output = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(output);
-        System.setOut(printStream);
+        ByteArrayOutputStream mockOutput = new ByteArrayOutputStream();
+        InputOutput inputOutput = new InputOutput(new PrintStream(mockOutput));
 
         inputOutput.welcomeMessage();
 
         assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n\n",
-                output.toString());
+                mockOutput.toString());
     }
 
     @Test
     public void shouldDisplayMenuOptions() {
-        InputOutput inputOutput = new InputOutput();
-        OutputStream output = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(output);
-        System.setOut(printStream);
+        ByteArrayOutputStream mockOutput = new ByteArrayOutputStream();
+        InputOutput inputOutput = new InputOutput(new PrintStream(mockOutput));
 
         inputOutput.displayMenu();
 
         assertEquals("[Menu Options] (Please type a number)\n[0] Quit Application\n[1] List of books\n[2] Checkout " +
                         "book\n[3] Return book\n",
-                output.toString());
-    }
-
-    @Test
-    public void shouldGetInputFromUser() {
-        InputOutput inputOutput = new InputOutput();
-        String userInput = "1";
-        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
-
-        String choice = inputOutput.getInputFromUser();
-
-        assertEquals(choice, "1");
+                mockOutput.toString());
     }
 
     @Test
     public void shouldDisplayBookList() {
-        InputOutput inputOutput = new InputOutput();
-        OutputStream output = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(output);
-        System.setOut(printStream);
+        ByteArrayOutputStream mockOutput = new ByteArrayOutputStream();
+        InputOutput inputOutput = new InputOutput(new PrintStream(mockOutput));
 
         inputOutput.displayBookList(books);
 
         assertEquals("Hamlet | William Shakespeare | 1603\nRomeo & Juliet | William Shakespeare | 1597\nMerchant of " +
-                "Venice | William Shakespeare | 1600\n", output.toString());
+                "Venice | William Shakespeare | 1600\n", mockOutput.toString());
     }
-
 }
