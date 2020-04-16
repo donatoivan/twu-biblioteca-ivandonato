e54e7884240471cf8db2e3eeba91db5c23f69d86
Ivan Donato <donato.ivan@gmail.com>
2020-04-17 00:51:41 +1000

    [ivan][#2.1] added displayMovies test, implemente code to make test pass

--- src/com/biblioteca/ivandonato/ApplicationBuilder.java
+++ src/com/biblioteca/ivandonato/ApplicationBuilder.java
@@ -3,7 +3,7 @@ package com.biblioteca.ivandonato;
 import java.util.ArrayList;
 
 public class ApplicationBuilder {
-    Biblioteca build(ArrayList books) {
-        return new Biblioteca(books, new Librarian(new InputOutput(System.out)));
+    Biblioteca build(ArrayList books, ArrayList movies) {
+        return new Biblioteca(books, movies, new Librarian(new InputOutput(System.out)));
     }
 }
