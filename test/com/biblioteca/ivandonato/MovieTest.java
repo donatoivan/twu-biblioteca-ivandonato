f9fda45f58492285b2b2eeaa275eec3217560fc2
Ivan Donato <donato.ivan@gmail.com>
2020-04-16 23:57:01 +1000

    [ivan][#2.1] added Movietest, shouldGetMovieTitle test

---
+++ test/com/biblioteca/ivandonato/MovieTest.java
@@ -0,0 +1,12 @@
+package com.biblioteca.ivandonato;
+
+import org.junit.Test;
+
+public class MovieTest {
+    @Test
+    public void shouldGetMovieTitle() {
+        Movie movie = new Movie("Mad Max");
+    }
+
+
+}
