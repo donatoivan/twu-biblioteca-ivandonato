78306a5d7c46f46b973326e28e6a052247d0e9a8
Ivan Donato <donato.ivan@gmail.com>
2020-04-17 00:00:38 +1000

    [ivan][#2.1] implmented Movie title, getter to make shouldGetMovieTitle pass

---
+++ src/com/biblioteca/ivandonato/Movie.java
@@ -0,0 +1,13 @@
+package com.biblioteca.ivandonato;
+
+public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
--- test/com/biblioteca/ivandonato/MovieTest.java
+++ test/com/biblioteca/ivandonato/MovieTest.java
@@ -2,11 +2,14 @@ package com.biblioteca.ivandonato;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MovieTest {
+
     @Test
     public void shouldGetMovieTitle() {
         Movie movie = new Movie("Mad Max");
+        assertEquals("Mad Max", movie.getTitle());
     }
-
 
 }
