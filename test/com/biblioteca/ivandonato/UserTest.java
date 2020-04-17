ed80769e2511cdc205de26f1567d0082aa637c64
Ivan Donato <donato.ivan@gmail.com>
2020-04-17 11:08:03 +1000

    [ivan][#2.3] added UserTest class, shouldGetuserId test

---
+++ test/com/biblioteca/ivandonato/UserTest.java
@@ -0,0 +1,11 @@
+package com.biblioteca.ivandonato;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+    @Test
+    public void shouldGetUserId() {
+        User user = new User("1234567");
+    }
+}
