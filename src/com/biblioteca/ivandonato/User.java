d74931e289a6005c8c38eb7f3b5450af0ee32e5e
Ivan Donato <donato.ivan@gmail.com>
2020-04-17 11:54:10 +1000

    [ivan][#2.3] implemented test to add book to myBooks attribute in User object

--- src/com/biblioteca/ivandonato/User.java
+++ src/com/biblioteca/ivandonato/User.java
@@ -2,12 +2,18 @@ package com.biblioteca.ivandonato;
 
 public class User {
     private String id;
+    private String password;
 
-    public User(String id) {
+    public User(String id, String password) {
         this.id = id;
+        this.password = password;
     }
 
     public String getUserId() {
         return id;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
--- test/com/biblioteca/ivandonato/UserTest.java
+++ test/com/biblioteca/ivandonato/UserTest.java
@@ -8,15 +8,28 @@ public class UserTest {
 
     @Test
     public void shouldGetUserId() {
-        User user = new User("1234567");
+        User user = new User("1234567", "password123");
 
-        assertEquals("1234567", user.getUserId());
+        String id = user.getUserId();
+
+        assertEquals("1234567", id);
     }
 
     @Test
     public void shouldGetPassword() {
         User user = new User("1234567", "password123");
 
-        assertEquals("password123", user.getPassword());
+        String password = user.getPassword();
+
+        assertEquals("password123", password);
+    }
+
+    @Test
+    public void shouldAddBookToUser() {
+        User user = new User("1234567", "password123");
+
+        user.addBooktoUser(book);
+
+        assertEquals(1, user.myBooks.size());
     }
 }
