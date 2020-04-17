dfb4ab22666fac35555b3a288254ca1014d76b76
Ivan Donato <donato.ivan@gmail.com>
2020-04-17 11:10:12 +1000

    [ivan][#2.3] added id as user attribute, test passed

--- src/com/biblioteca/ivandonato/User.java
+++ src/com/biblioteca/ivandonato/User.java
@@ -1,1 +1,13 @@
+package com.biblioteca.ivandonato;
 
+public class User {
+    private String id;
+
+    public User(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return id;
+    }
+}
--- test/com/biblioteca/ivandonato/UserTest.java
+++ test/com/biblioteca/ivandonato/UserTest.java
@@ -1,11 +1,15 @@
 package com.biblioteca.ivandonato;
 
-import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class UserTest {
+
     @Test
     public void shouldGetUserId() {
         User user = new User("1234567");
+
+        assertEquals("1234567", user.getUserId());
     }
 }
